10af7af86a1b7bd24d86560c07308d5d59da818d
Emmanuel Vodor <emmanuelvodor@stratis.fr>
2017-06-21 09:56:45 +0200

    [TASK] Minor changes

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') or die;
 
 if (TYPO3_MODE === 'BE') {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][$_EXTKEY] =
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers']['flamingo'] =
         \Ubermanu\Flamingo\Command\FlamingoCommandController::class;
 }
 
